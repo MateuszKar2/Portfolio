725166406253963ce1806c94b41ff9c2404e9c72
MateuszKar2 <mateuszwasabi@gmial.com>
2024-09-22 22:10:50 +0200

    add files

--- src/hooks/Hobby.jsx
+++ src/hooks/Hobby.jsx
@@ -1,12 +1,14 @@
 
 
 
-
 const Hobby = () => {
+
+    const basket = require('./../assets/images/basket.png')
 
     return(
         <>
             <h3>Would you like to be part of my team?</h3>
+            <img src={basket} alt="Where is my team" width={200}></img>
         </>
     )
 }
