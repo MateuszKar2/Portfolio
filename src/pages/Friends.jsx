e5ec756892a0fdecb80e34f043c2708bd523bd5c
MateuszKar2 <mateuszwasabi@gmial.com>
2024-09-26 21:31:02 +0200

    add files

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from './../module/Friends.module.css';
+import DataFetching from "components/DataFetching";
 
 const Friends = () => {
 
     const [friends, setFriends] = useState('my Friends');
 
-
+    
 
 
     return(
-        <div>
-            <button className={styles.btn} type="button" onClick={() => {setFriends( 'here they are' )}}>
-                {friends}
+        <div className={styles.friends}>
+            <button className={styles.btn} type="button" onClick={() => {setFriends( friends )}}>
+                {friends} 
             </button>
+
         </div>
         // Połączyć się z bazą danych, jeżeli zostanie kliknięty button-friend
     )
