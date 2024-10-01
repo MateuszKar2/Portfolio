cd0ad3d113a44fbc32343b643e335a6421dd8be5
MateuszKar2 <mateuszwasabi@gmial.com>
2024-10-01 10:15:56 +0200

    add styles/myExperience

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -16,28 +16,51 @@ const Menu = () => {
                 I have also done module tests.
                 If you are looking for a responsible person who learns quickly and loves programming... Contact me and maybe we can come to an agreement and create something cool together
             </div>
-            {/* Nowy komponent- projekty na kursie */}
-            <div>
-                <p>Scrum Master</p>
-                    <SiHtml5/>
-                    <SiSass />
-                <link rel="stylesheet" href="https://potocki92.github.io/IceCream/" />
-                {/* co robiłem, jak wyglądał nasz projekt */}
+
+            <div className={styles.project__header}>
+                <p className={styles.project__position}>Scrum Master</p>
+                    <div className={styles.project__icons}>
+                        <SiHtml5/>
+                        <SiSass />
+                    </div>
+                    <a href="https://potocki92.github.io/IceCream/" target="_blank" rel="noopener noreferrer">
+                            https://potocki92.github.io/IceCream/
+                    </a>
             </div>
-            <div>
-                <p>Developer</p>
-                    <SiJavascript/>
-                    <SiSass />
-                    <SiHtml5/>
-                <link rel="stylesheet" href="https://rafallakomiec.github.io/filmoteka-goit-js-group-project/" />
-                {/* co robiłem, jak wyglądał nasz projekt */}
+
+            <div className={styles.project__text}>
+                Group project. We built a responsive website. We used the BEM methodology. We used "Parcel".
+                In this project I was the Scrum Master. I also designed the "Products" section for computers, tablets and phones.
+                I chaired group meetings and supported developers.
+                I presented the group project.
             </div>
-            <div>
-                <p>Team Leader</p>
-                    <SiReact/>
-                    <SiRedux/>
-                    <SiNodedotjs/>
-                    <SiMongodb/>
+
+            <div className={styles.project__header}>
+                <p className={styles.project__position}>Developer</p>
+                    <div className={styles.project__icons}>
+                        <SiJavascript/>
+                        <SiSass />
+                        <SiHtml5/>
+                    </div>
+                    <a href="https://rafallakomiec.github.io/filmoteka-goit-js-group-project/" target="_blank" rel="noopener noreferrer">
+                             https://rafallakomiec.github.io/filmoteka-goit-js-group-project/
+                    </a>
+            </div>
+            
+            <div className={styles.project__text}>
+                We created a web application that allows you to search for movies by title. Users can get detailed information about movies, such as description, rating, cast, and many more.
+                We used an external API from the TMDb database to fetch movie data. During the project, I used advanced JavaScript and applied the BEM methodology with the SASS preprocessor.
+                My tasks included implementing the modal, managing localStorage, and configuring the API.
+            </div>
+
+            <div className={styles.project__header}>
+                <p className={styles.project__position}>Team Leader</p>
+                    <div className={styles.project__icons}>
+                        <SiReact/>
+                        <SiRedux/>
+                        <SiNodedotjs/>
+                        <SiMongodb/>
+                    </div>
                 <link rel="stylesheet" href="" />
                 {/* co robiłem, jak wyglądał nasz projekt */}
             </div>
