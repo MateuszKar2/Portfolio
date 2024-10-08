491f561a3a4f5561594b974bbab2258f95dddef7
MateuszKar2 <mateuszwasabi@gmial.com>
2024-10-08 16:32:50 +0200

    add files

--- src/pages/Hobby.jsx
+++ src/pages/Hobby.jsx
@@ -8,35 +8,32 @@ const Hobby = () => {
     return(
         <div className={styles.container}>
 
-            <div className={styles.traveling}>   
-                <h2 className={styles.traveling__title}>I love traveling</h2>
-                    <div className={styles.traveling__container}>
+            <div className={styles.hobby}>   
+                <h2 className={styles.hobby__title}>Travel</h2>
                             
-                        <div className={styles.traveling__text}>
+                        <div className={styles.hobby__text}>
                             When I have a vacation, I leave everyday life behind. I'm packing and going on a journey.
                             It may be my country or further away, to another continent.
                         </div>
-                    </div>
             </div>
 
-            <div className={styles.sports}>   
-                <h2 className={styles.sports__title}>I love sports</h2>
-                    <div className={styles.sports__container}>
+            <div className={styles.hobby}>   
+                <h2 className={styles.hobby__title}>Sports</h2>
                             
-                        <div className={styles.sports__text}>
-                            I love sports, competition and striving for perfection. Basketball, boxing, dancing. It's all part of me.
+                        <div className={styles.hobby__text}>
+                            I love sports, competition and striving for perfection. Basketball, boxing, dancing, swimming, archery. 
+                            It's a part of me that helps me work better.
                         </div>
-                    </div>
             </div>
 
-            <div className={styles.reading}>   
-                <h2 className={styles.reading__title}>I love reading books</h2>
-                    <div className={styles.reading__container}>
 
-                        <div className={styles.reading__text}>
+            <div className={styles.hobby}>   
+                <h2 className={styles.hobby__title}>Reading</h2>
+
+                        <div className={styles.hobby__text}>
                             When I finish work and need a break from writing code, I like to read a book.
+                            Books about programming, history, economics and 19th century novels
                         </div>
-                    </div>
             </div>
 
         </div>
