ae7248da69a5e64dbd5e3ae54fe3252288877339
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-03-07 16:05:42 +0100

    add page Not Found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,18 +1,13 @@
-import styles from './../module/NotFound.module.css';
 
 
 
 const NotFound = () => {
-
     return(
         <>
-            <div className={styles.message}>
-                <div>Do you know who you're looking for?</div>
-                <div>Do you know how much your time is worth?</div>
-            </div>
-            <div className={styles.link}>Mateusz Karpiński, Frontend Developer</div>
+            <h1>Not found</h1>
         </>
     )
 }
+
 
 export default NotFound;
