bc791fce90e756ca7366ccb0dbf10a61448fd41a
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-03-07 16:05:32 +0100

    add page Icons

---
+++ src/pages/Icons.jsx
@@ -0,0 +1,75 @@
+import {SiTypescript,SiJavascript,SiReact,SiRedux,SiNodedotjs,SiMongodb,SiHtml5,SiSass,SiCss3,SiGithub, SiVuedotjs, SiExpress} from 'react-icons/si';
+import styles from "./../module/Icons.module.css";
+
+export const Express = () => {
+    return (
+        <SiExpress className={styles.icon__express}/>
+    )
+}
+
+export const Vue = () => {
+    return (
+        <SiVuedotjs className={styles.icon__vue}/>
+    )
+}
+
+export const Github = () => {
+    return(
+        <SiGithub className={styles.icon__gitHub}/>
+    )
+}
+
+export const JavaScript = () => {
+    return(
+        <SiJavascript className={styles.icon__javaScript}/>
+    )
+}
+
+export const TypeScript = () => {
+    return(
+        <SiTypescript className={styles.icon__typeScript}/>
+    )
+}
+
+export const React = () => {
+    return(
+        <SiReact className={styles.icon__react}/>
+    )
+}
+
+export const Redux = () => {
+    return(
+        <SiRedux className={styles.icon__redux}/>
+    )
+}
+
+export const Node = () => {
+    return(
+        <SiNodedotjs className={styles.icon__node}/>
+    )
+}
+
+export const Mongo = () => {
+    return(
+        <SiMongodb className={styles.icon__mongo}/>
+    )
+}
+
+export const Html = () => {
+    return(
+        <SiHtml5 className={styles.icon__html}/>
+    )
+}
+
+export const Sass = () => {
+    return(
+        <SiSass className={styles.icon__sass}/>
+    )
+}
+
+export const Css = () => {
+    return(
+        <SiCss3 className={styles.icon__css}/>
+    )
+}
+
