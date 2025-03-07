41350ae4be32190aad84f6d11efe526315ed4b83
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-03-07 16:03:48 +0100

    add App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,21 @@
-
 import Cv from 'pages/Cv';
-import NotFound from "pages/NotFound";
-import Hobby from "pages/Hobby";
-import Projects from "pages/Projects";
-import { Routes, Route } from "react-router-dom";
-import Police from "pages/Police";
-import SharedLayout from "./SharedLayout";
-import Experience from 'pages/Experience';
-import University from 'pages/University';
-
-
+import SharedLayout from './SharedLayout';
+import NotFound from '../pages/NotFound';
+import Certificates from '../pages/Certificates';
+import Projects from '../pages/Projects';
+import About from '../pages/About';
+import { Routes, Route } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <>
-      <Routes>
-          <Route path="/" element={<SharedLayout/>}>
-          <Route index element={<Cv/>}/>
-          <Route path="hobby" element={<Hobby/>}/>
-          <Route path="experience" element={<Experience/>}>
-            <Route path="police" element={<Police/>}/>
-            <Route path="university" element={<University/>}/>
-          </Route>
-          <Route path="projects" element={<Projects/>}/>
-          <Route path="*" element={<NotFound/>}/>
-          </Route>
-        </Routes>
-        {/*hooki: useState, useEffect, useContext, useRef, useMemo */}
-        {/* 2. redux */}
-        {/* 3. przepisać na TS i dodać styled.ts */}
-        {/* 4. dodać testy */}
-    </>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Cv />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="certificates" element={<Certificates />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
-
-
-//Parametry URL
