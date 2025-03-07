4fb94d9f0bf8325da76380833e250bb75ac64774
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-03-07 16:04:57 +0100

    add page About

---
+++ src/pages/About.jsx
@@ -0,0 +1,49 @@
+import FreeTime from 'components/FreeTime';
+import styles from './../module/About.module.css';
+
+const police = require('./../assets/images/police.jpg');
+
+
+const About = () => {
+
+
+    return(
+    <>
+        <FreeTime />
+
+
+        <div className={styles.police}>
+
+        <h2 className={styles.police__title}>What did I do in the past?</h2>
+
+            <div className={styles.police__text}>
+                During my three years of service in the police, I developed <b className={styles.police__subtitle}>as Police officer:</b>
+            </div>
+
+            <div className={styles.police__position}>
+
+                <ul className={styles.police__description}>
+                    <li className={styles.police__positive}>Ability to work under pressure</li>
+                    <li className={styles.police__positive}>Effective problem-solving in crisis situations</li>
+                    <li className={styles.police__positive}>Analytical thinking</li>
+                    <li className={styles.police__positive}>Teamwork</li>
+                    <li className={styles.police__positive}>Effective communication</li>
+                    <li className={styles.police__positive}>Attention to detail</li>
+                    <li className={styles.police__positive}>Ability to work with processes and procedures</li>
+                </ul>
+
+                <img className={styles.police__images} src={police} alt="police"  />
+
+            </div>
+        </div>
+
+    </>
+
+    )
+}
+
+
+
+
+
+export default About;
