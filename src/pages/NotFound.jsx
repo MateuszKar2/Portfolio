bc5da81474d2a649f98fea0936e94f0334abf1bd
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-03-08 08:13:42 +0100

    add file notFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,14 @@
 
-
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-    return(
-        <>
-            <h1>Not found</h1>
-        </>
-    )
-}
-
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you're looking for doesn't exist.</p>
+      <Link to="/">Go back to home</Link>
+    </div>
+  );
+};
 
 export default NotFound;
